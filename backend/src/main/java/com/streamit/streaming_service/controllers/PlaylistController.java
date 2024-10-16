5ffe62cb2f2a709a49fc81f33c95de3e49697173
Samuel Rógenes <rogenescarvalho@hotmail.com>
2024-10-16 08:51:12 -0300

    fix playlist controller

--- backend/src/main/java/com/streamit/streaming_service/controllers/PlaylistController.java
+++ backend/src/main/java/com/streamit/streaming_service/controllers/PlaylistController.java
@@ -21,93 +21,112 @@ import java.nio.file.Paths;
 @AllArgsConstructor
 public class PlaylistController {
 
-    private final String baseDir = "caminho/Playlists";
+	private final String baseDir = "caminho/Playlists";
 
-    @GetMapping("/playlists")
-    public ResponseEntity<?> getPlaylist(
-            @RequestParam int number, 
-            @RequestParam String language, 
-            @RequestParam String mediaName, 
-            @RequestParam(required = false, defaultValue = "false") boolean includeSubtitle) {
+	@GetMapping("/playlists")
+	public ResponseEntity<?> getPlaylist(@RequestParam int number, @RequestParam String language,
+			@RequestParam String mediaName, @RequestParam(required = false) String subtitleLanguage) {
 
-        String playlistFileName;
-        String subtitleFileName;
+		String playlistFileName;
+		String subtitleFileName = null;
 
-        switch (language.toLowerCase()) {
-            case "pt": // Português
-                playlistFileName = String.format("playlist_pt%d.ts", number);
-                subtitleFileName = String.format("legenda_pt%d.vtt", number);
-                break;
-            case "eng": // Inglês
-                playlistFileName = String.format("playlist_eng%d.ts", number);
-                subtitleFileName = String.format("legenda_eng%d.vtt", number);
-                break;
-            case "es": // Espanhol
-                playlistFileName = String.format("playlist_es%d.ts", number);
-                subtitleFileName = String.format("legenda_es%d.vtt", number);
-                break;
-            case "fr": // Francês
-                playlistFileName = String.format("playlist_fr%d.ts", number);
-                subtitleFileName = String.format("legenda_fr%d.vtt", number);
-                break;
-            case "de": // Alemão
-                playlistFileName = String.format("playlist_de%d.ts", number);
-                subtitleFileName = String.format("legenda_de%d.vtt", number);
-                break;
-            case "zh": // Chinês
-                playlistFileName = String.format("playlist_zh%d.ts", number);
-                subtitleFileName = String.format("legenda_zh%d.vtt", number);
-                break;
-            case "jp": // Japonês
-                playlistFileName = String.format("playlist_jp%d.ts", number);
-                subtitleFileName = String.format("legenda_jp%d.vtt", number);
-                break;
-            case "it": // Italiano
-                playlistFileName = String.format("playlist_it%d.ts", number);
-                subtitleFileName = String.format("legenda_it%d.vtt", number);
-                break;
-            default:
-                return new ResponseEntity<>("Idioma inválido.", HttpStatus.BAD_REQUEST);
-        }
+		switch (language.toLowerCase()) {
+		case "pt":
+			playlistFileName = String.format("playlist_pt%d.ts", number);
+			break;
+		case "eng":
+			playlistFileName = String.format("playlist_eng%d.ts", number);
+			break;
+		case "es": // Espanhol
+			playlistFileName = String.format("playlist_es%d.ts", number);
+			break;
+		case "fr": // Francês
+			playlistFileName = String.format("playlist_fr%d.ts", number);
+			break;
+		case "de": // Alemão
+			playlistFileName = String.format("playlist_de%d.ts", number);
+			break;
+		case "zh": // Chinês
+			playlistFileName = String.format("playlist_zh%d.ts", number);
+			break;
+		case "jp": // Japonês
+			playlistFileName = String.format("playlist_jp%d.ts", number);
+			break;
+		case "it": // Italiano
+			playlistFileName = String.format("playlist_it%d.ts", number);
+			break;
+		default:
+			return new ResponseEntity<>("Idioma de áudio inválido.", HttpStatus.BAD_REQUEST);
+		}
 
-        // Construindo o caminho da pasta
-        String folderPath = String.format("%splaylists_%s/", baseDir, mediaName);
+		// Se o idioma da legenda for fornecido, construir o nome do arquivo de legenda
+		if (subtitleLanguage != null) {
+			switch (subtitleLanguage.toLowerCase()) {
+			case "pt":
+				subtitleFileName = String.format("legenda_pt%d.vtt", number);
+				break;
+			case "eng":
+				subtitleFileName = String.format("legenda_eng%d.vtt", number);
+				break;
+			case "es": // Espanhol
+				subtitleFileName = String.format("legenda_es%d.vtt", number);
+				break;
+			case "fr": // Francês
+				subtitleFileName = String.format("legenda_fr%d.vtt", number);
+				break;
+			case "de": // Alemão
+				subtitleFileName = String.format("legenda_de%d.vtt", number);
+				break;
+			case "zh": // Chinês
+				subtitleFileName = String.format("legenda_zh%d.vtt", number);
+				break;
+			case "jp": // Japonês
+				subtitleFileName = String.format("legenda_jp%d.vtt", number);
+				break;
+			case "it": // Italiano
+				subtitleFileName = String.format("legenda_it%d.vtt", number);
+				break;
+			default:
+				return new ResponseEntity<>("Idioma de legenda inválido.", HttpStatus.BAD_REQUEST);
+			}
+		}
 
-        // Lógica para obter a playlist .ts
-        try {
-            Path playlistPath = Paths.get(folderPath + playlistFileName);
-            Resource playlistResource = new UrlResource(playlistPath.toUri());
+		String folderPath = String.format("%splaylists_%s/", baseDir, mediaName);
 
-            if (playlistResource.exists()) {
-                HttpHeaders headers = new HttpHeaders();
-                headers.add(HttpHeaders.CONTENT_TYPE, "video/MP2T");
-                ResponseEntity<Resource> playlistResponse = new ResponseEntity<>(playlistResource, headers, HttpStatus.OK);
+		try {
+			// Lógica para obter a playlist .ts
+			Path playlistPath = Paths.get(folderPath + playlistFileName);
+			Resource playlistResource = new UrlResource(playlistPath.toUri());
 
-                // Se o cliente não requisitar a legenda, retorna apenas a playlist
-                if (!includeSubtitle) {
-                    return playlistResponse;
-                }
+			if (playlistResource.exists()) {
+				HttpHeaders headers = new HttpHeaders();
+				headers.add(HttpHeaders.CONTENT_TYPE, "video/MP2T");
 
-                // Lógica para obter a legenda .vtt
-                Path subtitlePath = Paths.get(folderPath + subtitleFileName);
-                Resource subtitleResource = new UrlResource(subtitlePath.toUri());
+				// Se não houver legenda, retornar apenas a playlist
+				if (subtitleFileName == null) {
+					return new ResponseEntity<>(playlistResource, headers, HttpStatus.OK);
+				}
 
-                if (subtitleResource.exists()) {
-                    HttpHeaders subtitleHeaders = new HttpHeaders();
-                    subtitleHeaders.add(HttpHeaders.CONTENT_TYPE, "text/vtt");
+				// Lógica para obter a legenda .vtt
+				Path subtitlePath = Paths.get(folderPath + subtitleFileName);
+				Resource subtitleResource = new UrlResource(subtitlePath.toUri());
 
-                    // Retorna um objeto que contém a playlist e a legenda
-                    return ResponseEntity.ok()
-                            .headers(headers)
-                            .body("Playlist disponível: " + playlistResource + ", Legenda disponível: " + subtitleResource);
-                } else {
-                    return new ResponseEntity<>("Legenda não encontrada.", HttpStatus.NOT_FOUND);
-                }
-            } else {
-                return new ResponseEntity<>("Playlist não encontrada.", HttpStatus.NOT_FOUND);
-            }
-        } catch (MalformedURLException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+				if (subtitleResource.exists()) {
+					HttpHeaders subtitleHeaders = new HttpHeaders();
+					subtitleHeaders.add(HttpHeaders.CONTENT_TYPE, "text/vtt");
+
+					// Retorna tanto a playlist quanto a legenda
+					return ResponseEntity.ok().headers(headers).body(
+							"Playlist disponível: " + playlistResource + ", Legenda disponível: " + subtitleResource);
+				} else {
+					return new ResponseEntity<>("Legenda não encontrada.", HttpStatus.NOT_FOUND);
+				}
+			} else {
+				return new ResponseEntity<>("Playlist não encontrada.", HttpStatus.NOT_FOUND);
+			}
+		} catch (MalformedURLException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
 }
