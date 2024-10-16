ea1b525c399485e77f1e19f4f8eb9053ed839f9f
Rafael Azevedo <rafaellucasaz94@gmail.com>
2024-10-16 16:06:57 -0300

    loginService fix

--- frontend/src/services/loginService.ts
+++ frontend/src/services/loginService.ts
@@ -1,5 +1,4 @@
-import { useNavigate } from "react-router-dom"
-
+import apiPath from "./apiService";
 
 type LoginData = {
     senha:string,
@@ -8,23 +7,51 @@ type LoginData = {
 
 
 
-const loginService = async(data:LoginData) =>{
+const loginService = async(loginData:LoginData,setErrors: React.Dispatch<React.SetStateAction<{ senha: string, email: string }>>) =>{
     try{
-        const response = await fetch('http://localhost:8080/api/login',{
+        const response = await fetch(`${apiPath}/login`,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
               },
-            body: JSON.stringify(data),
+            body: JSON.stringify(loginData),
         }); 
 
-        return response;
-     
+       
+        if(!response.ok){
+            const errorData = await response.json()
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                senha: errorData.errors.find((msg: string) => msg.toLowerCase().includes('senha')) || '',
+                email: errorData.errors.find((msg: string) => msg.toLowerCase().includes('email')) || '',
+            }));
+            return false;
+        }
+        const responseData = await response.json(); 
+        const token = responseData.token;
+        const idUser = responseData.idUser;
+        
+        localStorage.setItem('token', token); 
+        localStorage.setItem('idUser', idUser);
+        return true;
+        
+        
     }
     catch (error) {
         console.error('Erro ao realizar login:', error);
-        return undefined;
+        return false;;
     }   
 }
+
+
+// const checkUserType = async(idUser:number) => {
+
+//         const response = await fetch(`http://localhost:8080/api/users/${idUser}`);
+
+//         const responseData = await response.json();
+//         console.log(responseData);
+
+            
+// }
 
 export default loginService;
