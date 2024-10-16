fb230dae375b75c4a635c1dbe0645c31e4209cd5
Rafael Azevedo <rafaellucasaz94@gmail.com>
2024-10-15 21:05:51 -0300

    Profiles page endpoint connected

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,50 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import '../register/checkbox.css'
 import { Checkbox } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-const profiles = [
-    {
-        nome: "Rafael",
-        infantil: false,
-    },
-    {
-        nome: "Patrick",
-        infantil: false,
-    },
-    {
-        nome: "Samuca",
-        infantil: false,
-    }
-]
+import type { CheckboxProps } from 'antd';
 
-const genders = [ 
-    { label: 'Ação', value: 'acao' },
-    { label: 'Aventura', value: 'aventura' },
-    { label: 'Comédia', value: 'comedia' },
+interface Profile{
+    id:string,
+    nome:string,
+    iconUrl:string,
+    perfilInfantil:boolean,
+    generosPreferidos:string[],
+}
+
+const genres = [ 
+    { label: 'Ação', value: 'action' },
+    { label: 'Aventura', value: 'adventure' },
+    { label: 'Comédia', value: 'comedy' },
     { label: 'Drama', value: 'drama' },
-    { label: 'Terror', value: 'terror' },
-    { label: 'Suspense', value: 'suspense' },
-    { label: 'Ficção Científica', value: 'ficcao_cientifica' },
-    { label: 'Fantasia', value: 'fantasia' },
+    { label: 'Terror', value: 'horror' },
+    { label: 'Suspense', value: 'thriller' },
+    { label: 'Ficção Científica', value: 'science_fiction' },
+    { label: 'Fantasia', value: 'fantasy' },
     { label: 'Romance', value: 'romance' },
-    { label: 'Animação', value: 'animacao' },
+    { label: 'Animação', value: 'animation' },
     { label: 'Musical', value: 'musical' },
-    { label: 'Documentário', value: 'documentario' },
-    { label: 'Guerra', value: 'guerra' },
-    { label: 'Policial', value: 'policial' },
-    { label: 'Faroeste', value: 'faroeste' },
-    { label: 'Esporte', value: 'esporte' },
-    { label: 'Mistério', value: 'misterio' },
-    { label: 'Biografia', value: 'biografia' },
-    { label: 'Histórico', value: 'historico' },
-    { label: 'Filme Noir', value: 'filme_noir' }
+    { label: 'Documentário', value: 'documentary' },
+    { label: 'Guerra', value: 'war' },
+    { label: 'Policial', value: 'crime' },
+    { label: 'Faroeste', value: 'western' },
+    { label: 'Esporte', value: 'sports' },
+    { label: 'Mistério', value: 'mystery' },
+    { label: 'Biografia', value: 'biography' },
+    { label: 'Histórico', value: 'historical' },
   ];
 
 const Profile = () => {
-    const [userProfile,setProfile] = useState(1)  //estado inicial
+    const [userProfile,setProfile] = useState(1)  
+    const [userProfiles,setUserProfiles] = useState<Profile[]>([]);
     const [modal,setModal] = useState(false)
     const [name,setName] = useState<string>('')
-    const handleUserProfile = (n:number) =>{
+    const [selectedGenres,setSelectedGenres] = useState<string[]>([]);
+    const handleUserProfile = (n:number,id:string) =>{
+        localStorage.setItem('currentProfile',id)
         setProfile(n)
     }
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -52,19 +49,80 @@ const Profile = () => {
         setName(value);
       };
 
-      const newProfile = () =>{
-        profiles.push({nome:name,infantil:false})
+      const handleCheckbox:CheckboxProps['onChange'] = (e) => {
+        const { value, checked } = e.target;
+        setSelectedGenres((prevValues) => {
+          if (checked) {
+            return [...prevValues, value];
+          } 
+          return prevValues.filter((item) => item !== value);
+          
+        });
+      };
+
+      const getUserProfiles = async () => {
+        const id = localStorage.getItem('idUser');
+        try {
+            const response = await fetch(`http://localhost:8080/api/profiles/user/${id}`);
+            
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.log(errorData);
+            } else {
+                const profiles:Profile[] = await response.json(); 
+                setUserProfiles(profiles);
+                
+            }
+        } catch (error) {
+            console.error('Erro ao pegar perfis:', error);
+        }
+    };
+
+      const newProfile = async () =>{
+
+        const id = localStorage.getItem('idUser');
+
+        const data = {
+            "nome" : name,
+            "iconUrl": "",
+            "perfilInfantil": false,
+            generosPreferidos: selectedGenres,
+
+        }
+        try {
+            const response = await fetch(`http://localhost:8080/api/profiles/user/${id}`, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+        
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.log(errorData);
+            } else {
+               getUserProfiles()
+            }
+            } catch (error) {
+                console.error('Erro ao criar o usuário:', error);
+            }
         setModal(!modal)
       }
+
+      useEffect(() => {
+        
+        getUserProfiles()
+    }, []);
     return(
         <div className="w-screen h-screen bg-gradient-to-b from-black to-cyan-950 text-white flex flex-col flex-wrap items-center gap-20">
             <h1 className="text-[64px] mt-[50px]">Perfis de Usuário</h1>
             <div className="flex flex-row gap-24">
-                {profiles.map((profile,index) => {
+                {userProfiles.map((profile,index) => {
                     return(
                         <div className="flex flex-col items-center gap-5 hover:cursor-pointer  ">
                             <p className="text-[25px]">{profile.nome}</p> 
-                            <img className={`${userProfile == index ? "w-56" : "w-40"}`} src = {`${process.env.PUBLIC_URL}/profile_icon.png`} alt="icone de perfil" onClick={() => handleUserProfile(index)} />
+                            <img className={`${userProfile == index ? "w-56" : "w-40"}`} src = {`${process.env.PUBLIC_URL}/profile_icon.png`} alt="icone de perfil" onClick={() => handleUserProfile(index,profile.id)} />
                         </div>
                         
                     )
@@ -90,14 +148,14 @@ const Profile = () => {
                  <div className='w-full flex flex-col flex-wrap gap-1'>
                    <h3 className='self-center'>Gêneros preferidos</h3>
                    <div className='grid grid-cols-6 gap-1'>
-                   {genders.map((gender) =>(
+                   {genres.map((genre) =>(
                      <Checkbox 
-                     key={gender.value}
-                     value={gender.value}
-                     name = {gender.value}
-                     
+                     key={genre.value}
+                     value={genre.value}
+                     name = {genre.value}
+                     onChange={handleCheckbox}
                      className='custom-checkbox text-white col-span-2 items-center'
-                     >{gender.label}</Checkbox>
+                     >{genre.label}</Checkbox>
                    ))}
    
                    </div>
