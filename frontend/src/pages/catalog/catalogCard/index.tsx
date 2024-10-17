ab0b4830b8fb2c3045b4be1f75095fd2ea198c26
ErkPatrick <erick-patrickfreitas@outlook.com>
2024-10-17 08:15:49 -0300

    modal filme/serie

--- frontend/src/pages/catalog/catalogCard/index.tsx
+++ frontend/src/pages/catalog/catalogCard/index.tsx
@@ -3,6 +3,8 @@ import {Image } from 'antd';
 import '.././card.css'
 import cancelIcon from '../../../images/x_cancel.png';
 import { CardProps } from ".././types";
+import ModalFilme from "./ModalFilme";
+import ModalSerie from "./ModalSerie";
 
 
 const CatalogCard: React.FC<CardProps> = ({ title, description, releaseYear, duration, pictureUrl, last, first }) => {
@@ -33,7 +35,7 @@ const CatalogCard: React.FC<CardProps> = ({ title, description, releaseYear, dur
                 className={`mb-10 h-52 bg-slate-500 border-2 border-red-200 rounded-xl flex flex-row transition-all duration-300 
                     ${selected ? 'min-w-[400px] max-w-[400px] scale-105 overflow-hidden' : 'min-w-[200px] max-w-[200px]'}`}
             >
-                <div>
+                <div className="cursor-pointer">
                     <Image
                         width='197px'
                         height='204px'
@@ -53,30 +55,8 @@ const CatalogCard: React.FC<CardProps> = ({ title, description, releaseYear, dur
             </div>
 
             {showModal && (
-                <div
-                    className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
-                    onClick={handleCloseModal} // Fecha o modal ao clicar fora dele
-                >
-                    <div
-                        className="bg-gray-800 p-5 rounded-2xl flex flex-row "
-                        onClick={(e) => e.stopPropagation()} // Impede o clique dentro do modal de fechar
-                    >
-                        <div>
-                        <Image
-                            src={`${process.env.PUBLIC_URL}/batPost.jpg`}
-                            alt="a"
-                            preview={false}
-                            className="rounded-xl" // Mantém a borda arredondada
-                            style={{ width: '400px', height: '500px' }} // Força a imagem a se ajustar ao tamanho especificado
-                        />
-                        </div>
-                        <div className="w-[700px] mx-8">
-                            <h1 className="font-bold text-5xl mb-10">{title}</h1>
-                            <p className="text-[20px] text-justify">{description}</p>
-                        </div>
-                        <img className="ml-6 w-[30px] h-[30px] cursor-pointer" onClick={handleCloseModal} src={cancelIcon} alt="Close" />
-                    </div>
-                </div>
+                // <ModalFilme handleCloseModal={handleCloseModal} title={title} description={description} releaseYear={releaseYear} duration={duration} cancelIcon={cancelIcon}/>
+                <ModalSerie handleCloseModal={handleCloseModal} title={title} description={description} releaseYear={releaseYear} duration={duration} cancelIcon={cancelIcon}/>
             )}
         </>
     );
